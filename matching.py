7277097addfdc10c7d8f1ba95f977369cec2255e
HienTryToLearn <ht04929@georgiasouthern.edu>
2026-06-15 18:56:14 -0400

    Add initial structure and comments for matching.py

---
+++ matching.py
@@ -0,0 +1,33 @@
+#step 1 : clean and store data from input file into dictionaries for easy access later on in the algorithm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#step 2 : create a cheat sheet for the algorithm to quickly access the rank of a student for a hospital without having to loop through the preference list each time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#step 3 : implement Gale-Shapley Algorithm
